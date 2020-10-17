09fcd32524fdcaeec33be08dc9058a1e8642d9c9
Sagarika <sag23.bhandare@gmail.com>
2020-10-17 05:00:49 -0700

    A

---
+++ build/precache-manifest.658029747abe9ae279fa67dcdfe27fed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb39d116f00d2ea10586a0b95c4f49f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5801120f219751a0fb",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "8f4c9b019f5affcd341a",
+    "url": "/static/css/main.52047fbe.chunk.css"
+  },
+  {
+    "revision": "5c5801120f219751a0fb",
+    "url": "/static/js/2.14be5198.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.14be5198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4c9b019f5affcd341a",
+    "url": "/static/js/main.1fc376f2.chunk.js"
+  },
+  {
+    "revision": "0529a98c11a206755e39",
+    "url": "/static/js/runtime-main.99490353.js"
+  }
+]);
